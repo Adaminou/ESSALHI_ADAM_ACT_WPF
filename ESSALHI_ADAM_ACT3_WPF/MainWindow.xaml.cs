30ff23febb59f776b8b234d51e342dc11e86cce1
Essalhi Adam <200537@CCM-Asty-Moulin.local>
2023-11-10 16:14:39 +0100

    OUI

--- ESSALHI_ADAM_ACT3_WPF/MainWindow.xaml.cs
+++ ESSALHI_ADAM_ACT3_WPF/MainWindow.xaml.cs
@@ -22,25 +22,61 @@ namespace ESSALHI_ADAM_ACT3_WPF
     {
         public MainWindow()
         {
+
+           
             InitializeComponent();
+            BtnCalculer.Click += new RoutedEventHandler (BtnCalculer_Click);
+            BtnCalculer.MouseEnter += new MouseEventHandler(SurvolBouton);
             TxtA.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
             TxtB.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
             TxtC.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
 
         }
         public void VerifTextInput(Object sender, TextCompositionEventArgs e)
-        {          
+        {
 
             if (e.Text != "," && !EstEntier(e.Text))
             {
                 e.Handled = true;
             }
-
+            else if (e.Text != ",")
+            {
+                if (((TextBox)sender).Text.IndexOf(e.Text) > -1)
+                {
+                    e.Handled = true;
+                }
+        }
+ 
         }
         private bool EstEntier(string texteUser)
         {
-            string texteUser.TryParse
+            int n;
+
+            if (int.TryParse(texteUser, out n))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
+        private void BtnCalculer_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(TxtA.Text);
+            double b = double.Parse(TxtB.Text);
+            double c = double.Parse(TxtC.Text);
+            string type;
+            MethodesDuProjet mesOutils = new MethodesDuProjet();
+            mesOutils.ResoudTrinome(a, b, c, out type);
+
+        }
+        private void SurvolBouton(object sender, MouseEventArgs e)
+        {
+
+        }
+
+
 
 
 
